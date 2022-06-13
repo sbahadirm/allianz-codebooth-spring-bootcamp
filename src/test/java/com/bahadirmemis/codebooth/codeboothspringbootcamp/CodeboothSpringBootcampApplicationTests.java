7c55cca7ab88b52249b83d5e13ca1051c1148ed1
sbahadirm <sbahadirm@gmail.com>
2022-06-13 16:46:58 +0300

    Initial commit

---
+++ src/test/java/com/bahadirmemis/codebooth/codeboothspringbootcamp/CodeboothSpringBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahadirmemis.codebooth.codeboothspringbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeboothSpringBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
